e6ef80d258c293ab43a081a9aff6566af76770c3
constantine30 <banciu.constantin@gmail.com>
2022-11-10 16:34:50 +0000

    Tasks 1-4 done, Task 5 halfway done, highlights and image not done

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -9,7 +9,12 @@ import Post from '../Post';
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
+  return <main id="main">
+    {samplePosts.map(({postId, title, date, author, text}) => {
+      return <Post title={title} key={postId} date={date} author={author} text={text}
+      />
+    })}
+  </main>;
 }
 
 export default Main;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 import './index.css';
 
-function Post() {
-  return <article></article>;
+function Post({postId, title, date, author, text}) {
+  return <article>
+    <h1>{title}</h1>
+    <h2>{date}</h2>
+    <p>{author}</p>
+    <p>{text}</p>
+  </article>;
 }
 
 export default Post;
