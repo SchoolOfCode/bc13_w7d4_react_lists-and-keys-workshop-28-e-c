c6bf3a96f43a43b68aefaa31c1f091bf95242214
constantine30 <banciu.constantin@gmail.com>
2022-11-10 16:50:23 +0000

    Tasks 1-4 done, Task 5 halfway done, image not done

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -10,8 +10,8 @@ function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
   return <main id="main">
-    {samplePosts.map(({postId, title, date, author, text}) => {
-      return <Post title={title} key={postId} date={date} author={author} text={text}
+    {samplePosts.map(({postId, title, date, author, text, highlights}) => {
+      return <Post title={title} key={postId} date={date} author={author} text={text} highlights={highlights}
       />
     })}
   </main>;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import './index.css';
 
-function Post({postId, title, date, author, text}) {
+function Post({postId, title, date, author, text, highlights}) {
   return <article>
     <h1>{title}</h1>
     <h2>{date}</h2>
     <p>{author}</p>
     <p>{text}</p>
+    {highlights.map((highlight) => <li key={highlight.indexOf()}>{highlight}</li>)}
   </article>;
 }
 
